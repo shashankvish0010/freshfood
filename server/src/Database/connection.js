a589b58317e73ce50544dc475505ef3785e620bf
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-11-13 23:22:31 +0530

    Added Prod DB

--- server/src/Database/connection.js
+++ server/src/Database/connection.js
@@ -1,43 +1,6 @@
-const mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Mernfreshfood:freshfood@freshfood.wqw4jkq.mongodb.net/freshfood?retryWrites=true&w=majority"
-
-// mongoose.connect("mongodb+srv://Mernfreshfood:freshfood@freshfood.wqw4jkq.mongodb.net/freshfood?retryWrites=true&w=majority", { 
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-//   .then(() => {
-//     console.log("Database connected successfully");
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-client.connect().then(()=>{
-  client.db("admin").command({ ping: 1 }).then(()=>{
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  })
-}).catch((error)=>{
-  console.log(error);
-})
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// } 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI).then(()=>
+  console.log("FreshFood MongoDB Connected")
+).catch((error)=>console.log(error))
